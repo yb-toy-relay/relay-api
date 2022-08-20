45ad263a825830f0556689fd0b3b7a799da4e846
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-20 17:45:59 +0900

    init

---
+++ src/main/java/one/appscale/relayapi/RelayApiApplication.java
@@ -0,0 +1,13 @@
+package one.appscale.relayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelayApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/appscale/relayapi/RelayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.appscale.relayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
