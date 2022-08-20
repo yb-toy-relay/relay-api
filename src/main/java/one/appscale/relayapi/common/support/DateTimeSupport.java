3c5773031208ce0192afaaefb9394ae5acfab9eb
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-20 18:18:19 +0900

    common & config

---
+++ src/main/java/one/appscale/relayapi/common/support/DateTimeSupport.java
@@ -0,0 +1,8 @@
+package one.appscale.relayapi.common.support;
+
+import java.time.ZoneId;
+
+public class DateTimeSupport {
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'.'SSS";
+    public static final ZoneId ZONE_ID_SEOUL = ZoneId.of("Asia/Seoul");
+}
