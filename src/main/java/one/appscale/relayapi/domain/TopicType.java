9e5e8cbda33b87e0715905c0ebbbe1dddf01fb71
yaboong <daniel.yebon.yoo@gmail.com>
2022-08-20 22:11:08 +0900

    topic name change

--- src/main/java/one/appscale/relayapi/domain/TopicType.java
+++ src/main/java/one/appscale/relayapi/domain/TopicType.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum TopicType {
-    INSTALL_V1("install-v1"),
-    EVENT_V1("event-v1"),
-    REATTRIBUTION_V1("reattribution-v1");
+    INSTALL_V1("callback-install-v1"),
+    EVENT_V1("callback-event-v1"),
+    REATTRIBUTION_V1("callback-reattribution-v1");
 
     private final String topic;
 }
